725356d2908d57352d97e49a9123a07126998d1d
Yasuyuki Takeo <yasuflatland@gmail.com>
2024-08-14 12:39:22 +0900

    Initial

---
+++ README.md
@@ -0,0 +1,18 @@
+# Liferay Reference Archtecture Diagram
+Liferay's reference architecture and pipeline
+
+![Architecture](./diagram.png)
+
+# Precondition
+- Python 3.6 >=
+- [Graphviz](https://graphviz.gitlab.io/download/)
+- [Diagrams](https://diagrams.mingrammer.com/docs/getting-started/installation#quick-start)
+
+# Generate the network diagram
+
+## Install Diagram
+1. Install [Graphviz](https://graphviz.gitlab.io/download/)
+1. Install [Diagrams](https://diagrams.mingrammer.com/docs/getting-started/installation#quick-start)
+
+## Generate Image
+2. Run `python diagram.py`
---
+++ diagram.py
@@ -0,0 +1,51 @@
+from diagrams import Cluster, Diagram, Edge
+from diagrams.onprem.analytics import Spark
+from diagrams.onprem.compute import Server
+from diagrams.onprem.database import MySQL
+from diagrams.onprem.inmemory import Redis
+from diagrams.onprem.aggregator import Fluentd
+from diagrams.onprem.monitoring import Nagios
+from diagrams.onprem.network import Nginx
+from diagrams.onprem.queue import Kafka
+from diagrams.onprem.client import Users
+from diagrams.elastic.saas import Elastic
+from diagrams.custom import Custom
+from diagrams.generic.compute import Rack
+from diagrams.onprem.vcs import Github
+from diagrams.aws.network import ALB
+from diagrams.aws.compute import EC2
+from diagrams.aws.database import RDSMysqlInstance
+from diagrams.aws.management import Cloudwatch
+from diagrams.aws.storage import S3, Backup
+from diagrams.aws.engagement  import SES
+
+with Diagram("Liferay Reference Archtecture On-Prem",  filename="diagram", show=False):
+    users = Users("users")
+    github = Github("Repository")
+
+    with Cluster("Private Network"):
+        ingress = ALB("Load Balancer")
+        metrics = Cloudwatch("metrics")
+        backup = Backup("Backup")
+        mailsvc = SES("Email Service")
+
+        with Cluster("Liferay Cluster"):
+            lrsvc = [EC2("Liferay Node1"), EC2("Liferay Node2")]
+
+        with Cluster("Database Cluster"):
+            dbs = RDSMysqlInstance("Master and slave")
+            lrsvc >> dbs >> metrics
+
+        with Cluster("Elasticsearch Cluster"):
+            elks = Elastic("3 servers on EC2")
+            lrsvc >> elks >> metrics      
+
+        with Cluster("File Server"):
+            nfs = S3("Documents and Media")
+            lrsvc >> nfs >> metrics    
+
+    users >> ingress >> lrsvc
+    lrsvc - backup
+    nfs - backup
+    lrsvc - mailsvc
+    github >> Edge(label="Deploy") >> lrsvc
